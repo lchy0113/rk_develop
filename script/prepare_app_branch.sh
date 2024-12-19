e5d6b07bd03edd4d97775ffeb9460bc5824fd705
changyong Lee <lchy0113@gmail.com>
2024-12-19 18:35:55 +0900

    chore : Updat eprepare_app_branch.sh
    Change-Id: I8cded2cb1b11b6b7c8750d4ce9163c611acf656f

--- script/prepare_app_branch.sh
+++ script/prepare_app_branch.sh
@@ -2,17 +2,13 @@
 #cd vendor/kdiwin/apps/
 
 unset kd_apps
-unset branch_name
 for kd_apps in $( find . -maxdepth 1 -name "wallpad-*" -print )
 do
 	echo $kd_apps
-	cd $kd_apps
-    branch_name=`git branch | head -1 | awk '{print $NF}'`
-	cd -
-	git -C $kd_apps checkout $branch_name;
-	git -C $kd_apps branch --set-upstream-to=product/$branch_name $branch_name;
+	git -C $kd_apps checkout master ; 
+	git -C $kd_apps branch --set-upstream-to=apps/master master;
 	git -C $kd_apps pull ; 
 	git -C $kd_apps submodule init ; 
-	git -C $kd_apps submodule update ; 
+	git -C $kd_apps submodule update --init --recursive --force; 
 	git -C $kd_apps submodule sync ; 
 done
