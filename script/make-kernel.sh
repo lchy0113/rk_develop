3cb637f39f9b24f12d8be7cb799d817fdcfa1c62
changyong Lee <lchy0113@gmail.com>
2025-03-28 14:57:35 +0900

    chore: Update make-kernel.sh
    Change-Id: I635f3a84b6ea6c14992f6a489ac638816f6452dd

--- script/make-kernel.sh
+++ script/make-kernel.sh
@@ -7,7 +7,11 @@ ADDON_ARGS="CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
 KERNEL_ARCH="arm64"
 TARGET_DEVICE=`get_build_var TARGET_PRODUCT`
 KERNEL_DTS=`get_build_var PRODUCT_KERNEL_DTS`
-KERNEL_DEFCONFIG="rockchip_defconfig android-11.config non_debuggable.config disable_incfs.config kconfig.config $TARGET_DEVICE.config"
+PRODUCT_DEVICE=`get_build_var PRODUCT_DEVICE`
+KERNEL_DEFCONFIG="rockchip_defconfig android-11.config non_debuggable.config disable_incfs.config rk3568_poc.config $TARGET_DEVICE.config"
+NOVA_MODULE_KCONFIGS=`get_build_var NOVA_MODULE_KCONFIGS`
+
+CONFIG_DIR="./kernel/configs"
 
 echo -e "TARGET_DEVICE:"$TARGET_DEVICE
 echo -e "KERNEL_DTS:"$KERNEL_DTS
@@ -22,37 +26,45 @@ if [ -f ./logo_kernel.bmp.dev ]; then
 	cp logo_kernel.bmp.dev logo_kernel.bmp
 fi
 
-
-# target common kconfig
-if [ ! -f ./kernel/configs/kconfig.config ] || [ ! -e ./kernel/configs/kconfig.config ]; then
-	echo "...link kconfig.config"
-	cd ./kernel/configs/
-	rm kconfig.config
-	if [ "${TARGET_DEVICE:0:6}" = "rk3568" ]; then
-		ln -s ../../../device/kdiwin/test/common/configs/kconfig.config kconfig.config
-	else
-		ln -s ../../../device/kdiwin/newjeans/base/configs/kconfig.config
-	fi
-	cd -
+if [ ! -f "$CONFIG_DIR/$TARGET_DEVICE.config" ] || [ ! -e "$CONFIG_DIR/$TARGET_DEVICE.config" ]; then
+    echo "...link $TARGET_DEVICE.config"
+    cd "$CONFIG_DIR"
+    LINK_NAME="$CONFIG_DIR/$TARGET_DEVICE.config"
+    rm -f "$LINK_NAME"
+    if [[ $TARGET_DEVICE == rk3568_poc* ]]; then
+        ln -s "../../../device/kdiwin/test/rk3568_poc/configs/kconfig.$TARGET_DEVICE.config" "$LINK_NAME"
+    else
+        ln -s "../../../device/kdiwin/newjeans/$TARGET_DEVICE/configs/kconfig.config" "$LINK_NAME"
+    fi
+    echo "Created symlink: $LINK_NAME"
+    cd -
 fi
 
-# target device kconfig
-if [ ! -f ./kernel/configs/$TARGET_DEVICE.config ] || [ ! -e ./kernel/configs/$TARGET_DEVICE.config ]; then
-	echo "...link $TARGET_DEVICE.config"
-	cd ./kernel/configs/
-	rm $TARGET_DEVICE.config
-	if [ "${TARGET_DEVICE:0:6}" = "rk3568" ]; then
-		ln -s ../../../device/kdiwin/test/$TARGET_DEVICE/configs/kconfig.config $TARGET_DEVICE.config
-	else
-		ln -s ../../../device/kdiwin/newjeans/$TARGET_DEVICE/configs/kconfig.config $TARGET_DEVICE.config
-	fi
-	cd -
-fi
+for KCONFIG in $NOVA_MODULE_KCONFIGS; do
+    # Extract the base name of the kconfig file (e.g., wifi.kconfig -> wifi.config)
+    BASENAME=$(basename "$KCONFIG" .kconfig).config
+    LINK_NAME="$CONFIG_DIR/$BASENAME"
+
+    # Remove existing link if it exists
+    if [ -L "$LINK_NAME" ]; then
+        rm -f "$LINK_NAME"
+    fi
+
+    # Create a symbolic link with a relative path
+    ln -s "../../../$KCONFIG" "$LINK_NAME"
+    echo "Created symlink: $LINK_NAME -> ../../../$KCONFIG"
+
+    # Append the link to KERNEL_DEFCONFIG
+    KERNEL_DEFCONFIG+=" $BASENAME"
+done
 
 if [ ! -d $BUILD_PATH ]; then
 	mkdir $BUILD_PATH
 	chmod 777 $BUILD_PATH
 fi
+echo "=============================================================="
+echo $KERNEL_DEFCONFIG
+echo "=============================================================="
 
 if [ ! -f $BUILD_PATH/.config ]; then
 	echo "...defconfig"
@@ -98,12 +110,12 @@ cd ../u-boot && ./scripts/pack_resource.sh $BUILD_PATH/resource.img && cp resour
 BOOT_CMDLINE="console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init rootwait ro loop.max_part=7 androidboot.console=ttyFIQ0 androidboot.wificountrycode=KR androidboot.hardware=rk30board androidboot.boot_devices=fe310000.sdhci,fe330000.nandc androidboot.selinux=permissive buildvariant=userdebug"
 SECURITY_LEVEL="2022-03-05"
 
-mkbootfs -d ../out/target/product/$TARGET_PRODUCT/system ../out/target/product/$TARGET_PRODUCT/ramdisk | minigzip > ../out/target/product/$TARGET_PRODUCT/ramdisk.img 
+mkbootfs -d ../out/target/product/$PRODUCT_DEVICE/system ../out/target/product/$PRODUCT_DEVICE/ramdisk | minigzip > ../out/target/product/$PRODUCT_DEVICE/ramdisk.img 
 
 ## copy to aosp out directory
 #mkbootimg --kernel ../out/target/product/rk3568_poc/kernel --ramdisk ../out/target/product/rk3568_poc/ramdisk.img --dtb ../out/target/product/rk3568_poc/dtb.img --cmdline "$BOOT_CMDLINE" --os_version 12 --os_patch_level $SECURITY_LEVEL --second ./resource.img --header_version 2 --output ../out/target/product/rk3568_poc/boot.img
 ## copy to build directory
-mkbootimg --kernel $BUILD_PATH/arch/arm64/boot/Image --ramdisk ../out/target/product/$TARGET_DEVICE/ramdisk.img --dtb $BUILD_PATH/arch/arm64/boot/dts/rockchip/$KERNEL_DTS.dtb --cmdline "$BOOT_CMDLINE" --os_version 12 --os_patch_level $SECURITY_LEVEL --second $BUILD_PATH/resource.img --header_version 2 --output $BUILD_PATH/boot.img
+mkbootimg --kernel $BUILD_PATH/arch/arm64/boot/Image --ramdisk ../out/target/product/$PRODUCT_DEVICE/ramdisk.img --dtb $BUILD_PATH/arch/arm64/boot/dts/rockchip/$KERNEL_DTS.dtb --cmdline "$BOOT_CMDLINE" --os_version 12 --os_patch_level $SECURITY_LEVEL --second $BUILD_PATH/resource.img --header_version 2 --output $BUILD_PATH/boot.img
 
 
 echo ">>> done."
