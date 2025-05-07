da9ff94a49739b473404cce7a845c43b2dcdce0e
changyong Lee <lchy0113@gmail.com>
2025-05-07 10:55:29 +0900

    chore : Update check_popnoise.sh scripte
    Change-Id: I12344e2685d93eaaa4988dbf203f0bd30eedefe3

--- script/test_script/check_popnoise.sh
+++ script/test_script/check_popnoise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 초기 상태 설정
-prev_classd_state="off"
+prev_classd_state=">Off"
 
 # 모니터링 주기 (초 단위)
 SLEEP_INTERVAL=0.1
@@ -16,8 +16,8 @@ while true; do
     # 현재 ClassD Amp Enable 상태 읽기
     current_classd_state=$(adb shell "tinymix 'ClassD Amp Enable'" | grep -o '>On\|>Off')
 
-    # 조건: LineOut Amp1이 Off 상태이고, ClassD Amp Enable이 Off -> On으로 변경될 때 Pop Noise 발생
-    if [[ "$lineout_state" == ">Off" && "$current_classd_state" == ">On" ]]; then
+    # 조건: LineOut Amp1이 >Off 상태이고, ClassD Amp Enable이 >Off -> >On으로 변경될 때 Pop Noise 발생
+    if [[ "$lineout_state" == ">Off" && "$prev_classd_state" == ">Off" && "$current_classd_state" == ">On" ]]; then
         echo "[Pop Noise Detected] Timestamp: $(date)"
         echo "LineOut Amp1 State: $lineout_state"
         echo "Previous ClassD Amp State: $prev_classd_state, Current ClassD Amp State: $current_classd_state"
