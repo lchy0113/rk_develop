ebc5c6648c89b4d208bd644ce005c56f452e8f3f
changyong Lee <lchy0113@gmail.com>
2026-01-26 14:44:14 +0900

    chore : Update app checkout script
    Change-Id: Ia9f60a5f074dba74b9b9036ebc43edd4ffe18f0e

--- script/prepare_app_branch.sh
+++ script/prepare_app_branch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #cd vendor/kdiwin/apps/
 
+BRANCH="${1:-master}"
+
 unset kd_apps
 for kd_apps in $( find . -maxdepth 1 -type d -print )
 do
-	echo $kd_apps
-	git -C $kd_apps checkout master ; 
-	git -C $kd_apps branch --set-upstream-to=apps/master master;
-	git -C $kd_apps pull ; 
-	git -C $kd_apps submodule init ; 
-	git -C $kd_apps submodule update --init --recursive --force; 
-	git -C $kd_apps submodule sync ; 
+    echo $kd_apps
+    git -C $kd_apps checkout $BRANCH ;
+	git -C $kd_apps branch --set-upstream-to=apps/$BRANCH $BRANCH 2> /dev/null ;
+    git -C $kd_apps pull ;
+    git -C $kd_apps submodule init ;
+    git -C $kd_apps submodule update --init --recursive --force;
+    git -C $kd_apps submodule sync ;
 done
